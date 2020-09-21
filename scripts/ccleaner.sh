38066bda0389e972bf0032ec5bdbffcb4a6ae0de
Dante Barba <dantebarba@gmail.com>
2020-09-21 01:38:15 -0300

    restart all except for plexdrive

--- scripts/ccleaner.sh
+++ scripts/ccleaner.sh
@@ -12,5 +12,7 @@
 # logs cleaning
 
 # docker clean up
+/usr/bin/echo "INFO: Removing dangling images"
 /usr/bin/curl --silent https://gist.githubusercontent.com/macropin/3d06cd315a07c9d8530f/raw | /bin/bash -s rm-dangling
-/usr/bin/docker restart $(docker ps -q)
+/usr/bin/echo "INFO: Restarting running containers"
+/usr/bin/docker restart $(/usr/bin/docker ps | /usr/bin/grep -v "plexdrive" | /usr/bin/awk 'NR>1 {print $1}')
