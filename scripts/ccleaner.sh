0cc7c18b00428b943c475025ed75992a2b815665
Dante Barba <dantebarba@gmail.com>
2021-01-13 15:15:13 -0300

    typo

--- scripts/ccleaner.sh
+++ scripts/ccleaner.sh
@@ -20,5 +20,5 @@
 /usr/bin/docker start $(/usr/bin/docker ps -aq --filter 'label=com.docker.compose.project=plexdrive')
 /usr/bin/echo "INFO: Waiting 15s for virtual storage startup"
 /usr/bin/sleep 15s
-/usr/bin/echo "INFO: Restarting all containers containers"
+/usr/bin/echo "INFO: Restarting all containers"
 /usr/bin/docker start $(/usr/bin/comm -2 -3 <(/usr/bin/docker ps -aq | /usr/bin/sort) <(/usr/bin/docker ps -aq --filter 'label=com.docker.compose.project=plexdrive' | /usr/bin/sort))
