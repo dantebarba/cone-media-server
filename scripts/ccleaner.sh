37f14954f5ca9436c2ad78f1200cbe966cdbeaf4
Dante Barba <dantebarba@gmail.com>
2021-01-13 15:13:51 -0300

    added 15s sleep for better storage startup

--- scripts/ccleaner.sh
+++ scripts/ccleaner.sh
@@ -18,5 +18,7 @@
 /usr/bin/docker stop $(/usr/bin/docker ps -aq)
 /usr/bin/echo "INFO: Restarting virtual storage"
 /usr/bin/docker start $(/usr/bin/docker ps -aq --filter 'label=com.docker.compose.project=plexdrive')
+/usr/bin/echo "INFO: Waiting 15s for virtual storage startup"
+/usr/bin/sleep 15s
 /usr/bin/echo "INFO: Restarting all containers containers"
 /usr/bin/docker start $(/usr/bin/comm -2 -3 <(/usr/bin/docker ps -aq | /usr/bin/sort) <(/usr/bin/docker ps -aq --filter 'label=com.docker.compose.project=plexdrive' | /usr/bin/sort))
