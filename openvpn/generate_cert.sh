540a142aed02bac34645de4025242e802cb3ac04
Dante Barba <dantebarba@gmail.com>
2021-08-05 15:07:01 -0300

    generate certificate

---
+++ openvpn/generate_cert.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export CLIENTNAME="crown.***REMOVED***"
+# with a passphrase (recommended)
+docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME
+# without a passphrase (not recommended)
+docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME nopass
