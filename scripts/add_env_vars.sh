f618dedf408fbcb12befcd579b9e4fed13c066bb
Dante Barba <dantebarba@gmail.com>
2023-03-21 15:01:47 -0300

    pushing prod.env

--- scripts/add_env_vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'DUPLICATI_HOME=' >> /etc/environment
-echo 'DUPLICATI_CONFIG=' >> /etc/environment
-echo 'TRAEFIK_CONFIG=' >> /etc/environment
-echo 'FILEBROWSER_HOME=' >> /etc/environment
-echo 'FILEBROWSER_CONFIG=' >> /etc/environment
-echo 'DOCKER_MEDIA_SERVER' >> /etc/environment
-echo 'HEALTHCHECK_CONFIG=' >> /etc/environment
