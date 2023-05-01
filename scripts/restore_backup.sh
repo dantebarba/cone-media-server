de4d4a71a86985d398519ed016dd0c527d0b2c1f
Dante Barba <dantebarba@gmail.com>
2023-05-01 15:16:32 -0300

    add script to restore backup

---
+++ scripts/restore_backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# Check if the correct number of arguments were supplied
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <backup_file> <target_directory>"
+  exit 1
+fi
+
+# Get the backup file and target directory
+backup_file="$1"
+tgtdir="$2"
+
+# Create a temporary directory for the backup
+tmpdir=$(mktemp -d)
+
+# Extract the backup file to the temporary directory
+tar --extract --gzip --file="$backup_file" --directory="$tmpdir"
+
+# Restore /etc/environment
+etc_env_dump="${tmpdir}/etc_environment_dump.txt"
+cat ${etc_env_dump} > /etc/environment
+
+# Restore crontab
+crontab_file="${tmpdir}/crontab.txt"
+crontab "$crontab_file"
+
+# Restore the source directory
+srcdir_basename=$(basename "$srcdir")
+srcdir_tmpdir="${tmpdir}/${srcdir_basename}"
+mv "$srcdir_tmpdir" "$tgtdir"
+
+# Remove the temporary directory
+rm -rf "$tmpdir"
+
+# Print a message indicating that the restore was successful
+echo "Backup restored to $tgtdir"
